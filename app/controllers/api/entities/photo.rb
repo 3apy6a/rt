020790e5776b733fa6568fd57e56efcf314a6a51
3apy6a <zaruba.av@gmail.com>
2020-02-23 23:15:55 +0200

    add expose image

--- app/controllers/api/entities/photo.rb
+++ app/controllers/api/entities/photo.rb
@@ -1,15 +1,12 @@
 module Api
   module Entities
     class Photo < Grape::Entity
+
+      expose :image
       expose :created_at
-      expose :album_id, as: :related_album
       expose :related_album do
         expose :album, merge: true, using: Api::Entities::Album
       end
     end
   end
 end
-# Url
-# Upload date
-# Author
-# Related album
