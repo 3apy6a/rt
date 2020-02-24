da351d6e4c138fe03161335302d3eba0ee391ea1
3apy6a <zaruba.av@gmail.com>
2020-02-24 22:17:23 +0200

    naming refactoring

--- app/controllers/api/entities/album_entity.rb
+++ app/controllers/api/entities/album_entity.rb
@@ -1,16 +1,19 @@
 module Api
   module Entities
     class Album < Grape::Entity
+
       expose :name, as: :album_name
       expose :updated_at, as: :last_change
-      # expose :photos_count
       expose :album_author do
         expose :user, merge: true, using: Api::Entities::User
+
+        # expose :count_of_photo do
+        #   expose :photos.count, merge: true, using: Api::Entities::Photo
+        # end
       end
     end
   end
 end
-
 # Album name
 # Amount of photos
 # Album author
