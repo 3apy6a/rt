b59a86a89eee77eb1b5acd442e8dbf7563c24c19
3apy6a <zaruba.av@gmail.com>
2020-02-26 11:56:27 +0200

    change entities

--- app/controllers/api/entities/album_entity.rb
+++ app/controllers/api/entities/album_entity.rb
@@ -1,12 +1,16 @@
 module Api
-  module Entities
-    class AlbumEntity < Grape::Entity
+  module Entities::AlbumEntity
+    class Short < Grape::Entity
 
       expose :name, as: :album_name
-      expose :updated_at, as: :last_change
       expose :album_author do
-        expose :user, merge: true, using: Api::Entities::UserEntity
+        expose :user, merge: true, using: Api::Entities::UserEntity::Short
       end
+    end
+
+    class Full < Short
+      expose :updated_at
+      expose :photos_count
     end
   end
 end
--- app/controllers/api/entities/photo_entity.rb
+++ app/controllers/api/entities/photo_entity.rb
@@ -1,13 +1,12 @@
 module Api
-  module Entities
-    class PhotoEntity < Grape::Entity
+  class Entities::PhotoEntity < Grape::Entity
 
-      expose :image
-      expose :created_at
-      expose :related_album do
-        expose :album, merge: true, using: Api::Entities::AlbumEntity
-      end
-
+    expose :image
+    expose :created_at
+    expose :related_album do
+      expose :album, merge: true, using: Api::Entities::AlbumEntity::Short
     end
+
   end
 end
+
